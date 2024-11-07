d6675d66c63ce5b8d289a186fbd4b41a1e2ec07a
Sujal Khantwal <sujalkhantwal28@gmail.com>
2024-11-07 19:15:34 +0530

    3- like post mutation added

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -6,14 +6,14 @@ import { FaTrash } from "react-icons/fa";
 import LoadingSpinner from "./LoadingSpinner.jsx"
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import {toast} from "react-hot-toast"
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
 	const {data: authUser} = useQuery({queryKey: ["authUser"]});
-	const QueryClient = useQueryClient();
-	const {mutate: deletePost, isPending} = useMutation({
+	const queryClient = useQueryClient();
+	const {mutate: deletePost, isPending: isDeleting} = useMutation({
 		mutationFn: async () =>{
 			try {
 				const res = await fetch(`/api/posts/${post._id}`,{
@@ -33,11 +33,45 @@ const Post = ({ post }) => {
 		onSuccess: () =>{
 			toast.success("Post deleted Successfully");
 			// invalidate the query to refetch the data
-			QueryClient.invalidateQueries({queryKey: ["posts"]})
+			queryClient.invalidateQueries({queryKey: ["posts"]})
+		}
+	})
+
+	const  {mutate: likePost, isPending: isLiking} = useMutation({
+		mutationFn: async () =>{
+			try {
+				const res = await fetch(`/api/posts/like/${post._id}`,{
+					method: "POST",
+				});
+				const data = await res.json();
+				if(!res.ok){
+					throw new Error(data.error || "Something went wrong")
+				}
+				return data
+			} catch (error) {
+				throw new Error(error)
+			}
+		},
+		onSuccess: (updatedLikes)=>{
+			// this is not best the best UX, bcz it will refetch all posts.
+			// queryClient.invalidateQueries({queryKey:["posts"]})
+			// instead update the cache directly for the post
+
+			queryClient.setQueryData(["posts"], (oldData) => {
+				return oldData.map(p => {
+					if(p._id === post._id){
+						return {...p,likes: updatedLikes}
+					}
+					return p;
+				})
+			})
+		},
+		onError: (error) =>{
+			toast.error(error.message)
 		}
 	})
 	const postOwner = post.user;
-	const isLiked = false;
+	const isLiked = post.likes.includes(authUser._id);
 
 	const isMyPost = authUser._id === post.user._id;
 
@@ -53,7 +87,10 @@ const Post = ({ post }) => {
 		e.preventDefault();
 	};
 
-	const handleLikePost = () => {};
+	const handleLikePost = () => {
+		if(isLiking) return;
+		likePost();
+	};
 
 	return (
 		<>
@@ -75,9 +112,9 @@ const Post = ({ post }) => {
 						</span>
 						{isMyPost && (
 							<span className='flex justify-end flex-1'>
-								{!isPending && <FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />}
+								{!isDeleting && <FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />}
 
-								{isPending && (
+								{isDeleting && (
 									<LoadingSpinner  size="sm"/>
 								)}
 							</span>
@@ -163,10 +200,11 @@ const Post = ({ post }) => {
 								<span className='text-sm text-slate-500 group-hover:text-green-500'>0</span>
 							</div>
 							<div className='flex gap-1 items-center group cursor-pointer' onClick={handleLikePost}>
-								{!isLiked && (
+								{isLiking && <LoadingSpinner size="sm" />}
+								{!isLiked && !isLiking && (
 									<FaRegHeart className='w-4 h-4 cursor-pointer text-slate-500 group-hover:text-pink-500' />
 								)}
-								{isLiked && <FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
+								{isLiked && !isLiking && <FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
 
 								<span
 									className={`text-sm text-slate-500 group-hover:text-pink-500 ${
