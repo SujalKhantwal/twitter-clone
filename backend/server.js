b6d1001855838041f091b7ae51fa2d01ffb20599
Sujal Khantwal <sujalkhantwal28@gmail.com>
2024-11-09 11:31:51 +0530

    4 - prepared our app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
@@ -18,6 +19,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({limit: "5mb"})); // to parse req.body
 app.use(express.urlencoded({extended: true})); // to parse form data(urlencoded)
@@ -27,6 +29,14 @@ app.use("/api/auth",authRoutes);
 app.use("/api/user",userRoutes);
 app.use("/api/posts",postRoutes);
 app.use("/api/notifications",notificationRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+    app.get("*", (req,res) =>{
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+    });
+}
 
 app.listen(PORT, ()=>{
     connectMongoDB();
